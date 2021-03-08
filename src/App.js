6a15042c0dd90bc61de76f42e1751709bd43ce34
Amritpal Singh <amritdbest94@gmail.com>
2021-03-07 22:47:18 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai'
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'fd2cdf3238fa42428443589bac2e38a3'
+})
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 75,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(
+        function(response) {
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        },
+        function(err) {
+          console.log(err);
+        }
+      )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onSubmit} 
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App'
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
-
 
 ReactDOM.render(
   <React.StrictMode>
